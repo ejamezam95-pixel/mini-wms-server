de1cd4cc6f2efcd91aa795f647cb5c83ae4c2720
ejamezam95-pixel <ejamezam95@gmail.com>
2025-08-14 14:54:47 +0800

    Add files via upload

--- server.js.js
+++ server.js.js
@@ -1,19 +1,17 @@
-
-```javascript
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Contoh endpoint
 app.get('/', (req, res) => {
-  res.send('Mini WMS Server Berjalan');
+    res.send('Server berjalan!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server berjalan pada port ${PORT}`);
+    console.log('Server berjalan pada port ' + PORT);
 });
-```
